a16713025dfd51cf3a77b8244e5d0c3e5e7d0e36
Jerry Dev <jerryarthur075@gmail.com>
2026-03-27 16:51:23 +0000

    fixed jar paths as Render runs runs service from /opt/render/project/src

--- runner/execute.js
+++ runner/execute.js
@@ -5,17 +5,18 @@ export function runJUnit(testCode) {
   return new Promise((resolve) => {
     fs.writeFileSync("/tmp/SolutionTest.java", testCode);
 
+    const junit = "/opt/render/project/src/runner/junit.jar";
+    const hamcrest = "/opt/render/project/src/runner/hamcrest.jar";
+
     const cmd = `
-      javac -cp ./runner/junit.jar:./runner/hamcrest.jar:/tmp /tmp/SolutionTest.java &&
-      java -cp ./runner/junit.jar:./runner/hamcrest.jar:/tmp org.junit.runner.JUnitCore SolutionTest
+      javac -cp ${junit}:${hamcrest}:/tmp /tmp/SolutionTest.java &&
+      java -cp ${junit}:${hamcrest}:/tmp org.junit.runner.JUnitCore SolutionTest
     `;
 
     exec(cmd, (err, stdout, stderr) => {
       if (err) {
         return resolve(stderr || err.message);
       }
-
-      // JUnit prints to stderr even when passing
       resolve(stdout + stderr);
     });
   });
