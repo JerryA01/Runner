d59166fa1f818bfb0b35d5f8db3bb56577bcbdc4
Jerry Dev <jerryarthur075@gmail.com>
2026-03-27 15:35:04 +0000

    add real JUnit + Hamcrest jars and update classpath for Render

--- runner/execute.js
+++ runner/execute.js
@@ -3,11 +3,12 @@ import fs from "fs";
 
 export function runJUnit(testCode) {
   return new Promise((resolve) => {
+    // Write test file to /tmp
     fs.writeFileSync("/tmp/SolutionTest.java", testCode);
 
     const cmd = `
-      javac -cp ./runner/junit.jar:/tmp /tmp/SolutionTest.java &&
-      java -cp ./runner/junit.jar:/tmp org.junit.runner.JUnitCore SolutionTest
+      javac -cp ./runner/junit.jar:./runner/hamcrest.jar:/tmp /tmp/SolutionTest.java &&
+      java -cp ./runner/junit.jar:./runner/hamcrest.jar:/tmp org.junit.runner.JUnitCore SolutionTest
     `;
 
     exec(cmd, (err, stdout, stderr) => {
