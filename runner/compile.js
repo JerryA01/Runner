2020f5010ae0117afb5b689010dbfa57ac6a67f7
Jerry Dev <jerryarthur075@gmail.com>
2026-03-27 15:10:39 +0000

    fixed compile time error with user's code being saved into Main instead of Solution

--- runner/compile.js
+++ runner/compile.js
@@ -3,9 +3,9 @@ import fs from "fs";
 
 export function compileJava(code) {
   return new Promise((resolve) => {
-    fs.writeFileSync("/tmp/Main.java", code);
+    fs.writeFileSync("/tmp/Solution.java", code);
 
-    exec("javac /tmp/Main.java", (err, stdout, stderr) => {
+    exec("javac /tmp/Solution.java", (err, stdout, stderr) => {
       if (err || stderr) {
         return resolve({ error: stderr || err.message });
       }
--- runner/execute.js
+++ runner/execute.js
@@ -3,11 +3,11 @@ import fs from "fs";
 
 export function runJUnit(testCode) {
   return new Promise((resolve) => {
-    fs.writeFileSync("/tmp/MainTest.java", testCode);
+    fs.writeFileSync("/tmp/SolutionTest.java", testCode);
 
     const cmd = `
-      javac -cp ./runner/junit.jar:/tmp /tmp/MainTest.java &&
-      java -cp ./runner/junit.jar:/tmp org.junit.runner.JUnitCore MainTest
+      javac -cp ./runner/junit.jar:/tmp /tmp/SolutionTest.java &&
+      java -cp ./runner/junit.jar:/tmp org.junit.runner.JUnitCore SolutionTest
     `;
 
     exec(cmd, (err, stdout, stderr) => {
